a411531f29a82826864e22cf1573ed93f895e0c5
Usuario <Usuario@CPD03-M03>
2024-07-30 12:10:33 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoWindowsForms1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ProjetoWindowsForms1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.N1 = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.N2 = new System.Windows.Forms.Label();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // N1
+            // 
+            this.N1.AutoSize = true;
+            this.N1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N1.Location = new System.Drawing.Point(58, 95);
+            this.N1.Name = "N1";
+            this.N1.Size = new System.Drawing.Size(87, 18);
+            this.N1.TabIndex = 0;
+            this.N1.Text = "Número 1:";
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(146, 93);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(585, 20);
+            this.txtN1.TabIndex = 1;
+            this.txtN1.TextChanged += new System.EventHandler(this.txtN1_TextChanged);
+            // 
+            // N2
+            // 
+            this.N2.AutoSize = true;
+            this.N2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.N2.Location = new System.Drawing.Point(58, 142);
+            this.N2.Name = "N2";
+            this.N2.Size = new System.Drawing.Size(87, 18);
+            this.N2.TabIndex = 2;
+            this.N2.Text = "Número 2:";
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(146, 140);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(585, 20);
+            this.txtN2.TabIndex = 3;
+            this.txtN2.TextChanged += new System.EventHandler(this.txtN2_TextChanged);
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(110, 225);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(137, 66);
+            this.btnSomar.TabIndex = 4;
+            this.btnSomar.Text = "Somar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(341, 225);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(137, 66);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(564, 225);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(137, 66);
+            this.btnFechar.TabIndex = 6;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(57, 387);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(96, 20);
+            this.lbl1.TabIndex = 7;
+            this.lbl1.Text = "Resultado:";
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(183, 387);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 20);
+            this.lblResultado.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 470);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.N2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.N1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label N1;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.Label N2;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ ProjetoWindowsForms1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoWindowsForms1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            // Entrada de Dados
+            int n1 = int.Parse(txtN1.Text);
+            int n2 = int.Parse(txtN2.Text);
+
+            // Processamento
+            int resultado = n1 + n2;
+
+            // Saída de Dados
+            lblResultado.Text = resultado.ToString();
+        }
+
+        private void txtN1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtN2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            // Limpa as caixas de texto através da função Clear
+            txtN1.Clear();
+            txtN2.Clear();
+
+            // Move o texto Zero para a Label
+            lblResultado.Text = "0";
+
+            // Move o cursor para o TextBox
+            txtN1.Focus();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            // Fecha o formulário
+            Close();
+        }
+    }
+}
